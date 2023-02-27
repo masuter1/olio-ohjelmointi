de91ccb538c7f67481363dccecdcb9814c71b51f
s2101048 <2101048@edu.bc.fi>
2023-02-27 13:55:20 +0200

    ei valmis. t3 luokka luotu

---
+++ t3-nuolia-kaupan/t3-nuolia-kaupan/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Nuoli
+{
+    private Head _head;
+    private Fletching _fletching;
+    private int _shaft;
+    public Nuoli(int shaft, Head head, Fletching fletching)
+    {
+        _shaft = shaft;
+        _head = head;
+        _fletching = fletching;
+    }
+    public double Hinta()
+    {
+        double hinta = 0;
+        switch (_head)
+        {
+            case Head.timantti: hinta += 50; break;
+            case Head.teräs: hinta += 5; break;
+            case Head.puu: hinta += 3; break;
+            default: hinta += 0; break;
+        }
+        switch (_fletching)
+        {
+            case Fletching.lehti: hinta += 0; break;
+            case Fletching.kanansulka: hinta += 1; break;
+            case Fletching.kotkansulka: hinta += 5; break;
+            default: hinta += 0; break;
+        }
+        hinta += _shaft * 0.05;
+        return hinta;
+    }
+}
+internal enum Head { puu, teräs, timantti };
+internal enum Fletching { lehti, kanansulka, kotkansulka };
