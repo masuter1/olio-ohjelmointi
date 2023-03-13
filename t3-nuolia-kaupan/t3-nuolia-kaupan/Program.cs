185198a56be71f252a8c2b893e5ed52e0801859e
s2101048 <2101048@edu.bc.fi>
2023-03-13 12:56:43 +0200

    t3 valmis

--- t3-nuolia-kaupan/t3-nuolia-kaupan/Program.cs
+++ t3-nuolia-kaupan/t3-nuolia-kaupan/Program.cs
@@ -1,5 +1,63 @@
 ﻿using System;
 
+int shaft = 0;
+Head head = Head.puu;
+Fletching fletching = Fletching.lehti;
+int kohta = 0;
+bool done = false;
+while (!done)
+{
+    switch (kohta)
+    {
+        case 0:
+            try
+            {
+                shaft = Convert.ToInt32(Question("Kuinka pitkä varsi? (60-100)"));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Annathan vain kokonaisia numeroita");
+                goto case 0;
+            }
+            if (59 > shaft || 101 < shaft) { Console.WriteLine("Annathan vain sallitun koon"); goto case 0; }
+            kohta++; break;
+        case 1:
+            try
+            {
+                head = Enum.Parse<Head>(Question("Minkälaisen kärjen haluat? (puu, teräs vai timantti)"), true);
+            }
+            catch
+            {
+                Console.WriteLine("Annathan vain yhden vaihtoehdoista");
+                goto case 1;
+            }
+            kohta++; break;
+        case 2:
+            try
+            {
+                fletching = Enum.Parse<Fletching>(Question("Minkälaisen sulan haluat? (lehti, kanansulka vai kotkansulka)"));
+            }
+            catch
+            {
+                Console.WriteLine("Annathan vain yhden vaihtoehdoista");
+                goto case 2;
+            }
+            kohta++; break;
+        default: done = true; break;
+
+    }
+}
+
+
+Nuoli nuoli = new Nuoli(shaft, head, fletching);
+
+Console.Write($"Tämä nuoli maksaa {nuoli.Hinta()} kultakolikkoa");
+
+string Question(string question)
+{
+    Console.WriteLine(question); return Console.ReadLine();
+}
+
 class Nuoli
 {
     private Head _head;
@@ -32,5 +90,6 @@ class Nuoli
         return hinta;
     }
 }
+
 internal enum Head { puu, teräs, timantti };
 internal enum Fletching { lehti, kanansulka, kotkansulka };
