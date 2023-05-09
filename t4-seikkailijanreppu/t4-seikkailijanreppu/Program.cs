acd9118f7bbae9dd082a0066cb607ae5474686d5
s2101048 <2101048@edu.bc.fi>
2023-05-09 12:57:46 +0300

    tehtävä 4 kesken ja aloitettu

---
+++ t4-seikkailijanreppu/t4-seikkailijanreppu/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Item
+{
+    protected double _weight;
+    protected double _size;
+    protected string _name;
+
+    public Item(double weight, double size, string name)
+    {
+        this._weight = weight;
+        this._size = size;
+        this._name = name;
+    }
+    // Haut
+    public double AnnaPaino()
+    {
+        return _weight;
+    }
+    public double AnnaKoko()
+    {
+        return _size;
+    }
+    public string AnnaNimi()
+    {
+        return _name;
+    }
+    // Asetukset
+    public void AsetaPaino(double weight)
+    {
+        _weight = weight;
+    }
+    public void AsetaKoko(double size)
+    {
+        _size = size;
+    }
+    public void AsetaNimi(string name)
+    {
+        _name = name;
+    }
+    // Printtaukset
+    public virtual string ItemInfo()
+    {
+        string tiedot = $"{_name} - Paino: {_weight} Koko: {_size}";
+        return tiedot;
+    }
+}
+
+public class Arrow : Item
+{
+    // tee tavara luokat
+}
